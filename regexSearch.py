4024193c2fd9182d35c4c2b8a0d639c10f9ddcca
jvbradley <jvbradley@users.noreply.github.com>
2020-07-12 13:53:53 -0700

    Initial file creation
    Second independent project for chapter 9

---
+++ regexSearch.py
@@ -0,0 +1,12 @@
+#! python3
+# https://automatetheboringstuff.com/2e/chapter9/
+'''
+Write a program that opens all .txt files in a folder and searches for any line
+that matches a user-supplied regular expression. The results should be printed
+to the screen.
+'''
+
+def regexSearch():
+    print('Hello, world!')
+
+regexSearch()
